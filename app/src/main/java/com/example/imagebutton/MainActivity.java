fbe000151ae33ee983eea06718f3ff73ba921212
hl3646 <hl3646@bs.amalnet.k12.il>
2022-02-10 01:13:16 +0200

    added images to drawble folder and started the on click

--- app/src/main/java/com/example/imagebutton/MainActivity.java
+++ app/src/main/java/com/example/imagebutton/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.imagebutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView iv;
+    ImageButton ib;
+    int random = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv = (ImageView) findViewById(R.id.imageView);
+        ib = (ImageButton) findViewById(R.id.imageButton);
+    }
+
+    public void randomNumber(View view)
+    {
+        random = (int)(Math.random()* (3-1))+1;
+        if(random == 1)
+        {
+            iv.setImageResource(R.drawable.num1);
+            ib.setForeground(R.drawable.one);
+        }
+        else if(random == 2)
+        {
+            iv.setImageResource(R.drawable.num2);
+        }
+        else
+        {
+            iv.setImageResource(R.drawable.num3);
+        }
     }
 }
